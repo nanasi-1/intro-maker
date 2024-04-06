740fa9a5c9ed15e4723773e3d18aeafab34c3999
nanasi-1 <test-email>
2024-04-06 10:36:28 +0900

    ループを追加

--- toImage.js
+++ toImage.js
@@ -1,16 +1,31 @@
 console.log('Hello World!');
 
-const main = async () => {
-  const img = new Image();
-  img.src = await domtoimage.toSvg(head);
-  
+const getCanvas = () => {
   /** @type {HTMLCanvasElement} */
   const canvas = document.getElementById('root');
   const ctx = canvas.getContext('2d');
-  costume.append(img);
+  return {canvas, ctx};
+}
 
-  ctx.drawImage(img, 0, 0);
-  console.log('done');
+const getCostumes = async () => {
+  const costumeElem = document.getElementById('costume');
+  const costumes = [];
+  for (const elem of costumeElem.children) {
+    const img = new Image();
+    img.src = await domtoimage.toSvg(elem);
+    costumes.push(img);
+  }
+  costumeElem.remove();
+  return costumes;
+}
+
+const main = async () => {
+  const {ctx} = getCanvas();
+  const costumes = await getCostumes();
+  for (const costume of costumes) {
+    console.log('draw', costume);
+    ctx.drawImage(costume, 0, 0);
+  }
 };
 
 try {
