315c5adf01956b65a6e74d7252a8ad35528b4338
nanasi-1 <test-email>
2024-04-06 21:44:22 +0900

    使用検討中

--- scripts/toImage.js
+++ scripts/toImage.js
@@ -6,7 +6,12 @@ const getCanvas = () => {
   /** @type {HTMLCanvasElement} */
   const canvas = document.getElementById('root');
   const ctx = canvas.getContext('2d');
-  return {canvas, ctx};
+  const size = {w: canvas.width, h: canvas.height};
+  return {
+    canvas, ctx, 
+    canvasX: canvas.width / 2,
+    canvasY: canvas.height / 2
+  };
 }
 
 const getCostumes = async () => {
@@ -19,21 +24,21 @@ const getCostumes = async () => {
 }
 
 const main = async () => {
-  const {ctx, canvas} = getCanvas();
+  const {ctx, canvas, canvasX, canvasY} = getCanvas();
   const sprite = new Sprite(canvas, ctx);
 
   const costumes = await getCostumes();
   const background = sprite.add(costumes.background);
   const box = sprite.add(costumes.box);
   const circle = sprite.add(costumes.circle);
-
   document.getElementById('costume').remove();
+
+  box.to(-canvasX, -canvasY);
   
   while (true) {
-    for (let i = 0; i < 50; i++) {
+    for (let i = 100; i > 20; i -= (i - 10) / 10) {
       const size = i / 100;
-      circle.writeStyle('transform', `scale(${size})`);
-      circle.moveX(-1);
+      circle.writeStyle('scale', size);
       await sleep(30);
     }
 
@@ -41,25 +46,25 @@ const main = async () => {
       box.moveX(x * 1.55);
       await sleep(30);
     }
-    box.toX(canvas.width - 100);
+    box.toX(canvas.width / 2);
 
     for (let y = 40; y > 0 && box.coordinate.y < canvas.height - 100; y--) {
       box.moveY(y / 1.2);
       await sleep(30);
     }
-    box.toY(canvas.height - 100);
+    box.toY(canvas.height / 2);
 
-    for (let x = 30; x > 0 && box.coordinate.x > 0; x--) {
+    for (let x = 30; x > 0 && box.coordinate.x > -canvas.width; x--) {
       box.moveX(-x * 1.55);
       await sleep(30);
     }
-    box.toX(0);
+    box.toX(-canvas.width / 2);
 
-    for (let y = 40; y > 0 && box.coordinate.y > 0; y--) {
+    for (let y = 40; y > 0 && box.coordinate.y > -canvas.height; y--) {
       box.moveY(-y / 1.2);
       await sleep(30);
     }
-    box.toY(0);
+    box.toY(-canvas.height / 2);
   }
 };
 
@@ -68,3 +73,14 @@ try {
 } catch (e) {
   console.error('エラーが発生しました', e);
 }
+
+/*
+# キャンバスの端への移動について
+- sizeHackモードを作って、キャンバスの外には出ないようにする
+- 端に飛ぶメソッドを作る
+- その辺の数値が取得できるgetterを作る
+
+# サイズについて
+- domtoimageの挙動がかなり不安なため、サイズ関係のメソッドが欲しい
+- domtoimageに頼らない、ctxで描画するCloneがあってもいいかも
+ */
